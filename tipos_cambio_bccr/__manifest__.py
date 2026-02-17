99f2e11112acb988afee3dd8dc2239f56dfca79a
rovinlr <rovinlr@gmail.com>
2026-02-17 16:57:31 -0600

    Refactor Hacienda provider to res.currency.rate.provider service

--- tipos_cambio_bccr/__manifest__.py
+++ tipos_cambio_bccr/__manifest__.py
@@ -1,12 +1,11 @@
 {
     'name': 'Tipos de Cambio BCCR',
-    'version': '19.0.1.10.0',
+    'version': '19.0.2.0.0',
     'summary': 'Proveedor de tipos de cambio usando Hacienda de Costa Rica',
     'category': 'Accounting',
     'author': 'FenixCR Solutions',
     'license': 'LGPL-3',
     'depends': ['account', 'currency_rate_live'],
-    'data': ['views/res_config_settings_views.xml'],
     'installable': True,
     'application': False,
 }
